186609e302d411fee5f615c3915290f35382ccc9
delphvr <delphine.vanrossum@student.uclouvain.be>
2024-12-10 15:13:36 +0100

    ex1.1-1.2 couseinfo

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -29,7 +29,7 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <>
-      <p>Number of exercises {props.total}</p>;
+      <p>Number of exercises {props.total}</p>
     </>
   );
 };
