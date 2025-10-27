fa77e67992ca50977748e167abdf8c1004b3c9ca
Delphine <delphine.vanrossum@student.uclouvain.be>
2025-10-27 18:41:29 +0100

    ex1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,12 +3,28 @@ import { useState } from "react";
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
 const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = () => {
+    if (all == 0) {
+      return 0;
+    }
+    return (good + bad * -1) / all;
+  };
+  const positive = () => {
+    if (all == 0) {
+      return 0;
+    }
+    return good / all;
+  };
   return (
     <>
       <h1>Statistics</h1>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>All: {all}</p>
+      <p>Average: {average()}</p>
+      <p>Positive: {positive()}%</p>
     </>
   );
 };
