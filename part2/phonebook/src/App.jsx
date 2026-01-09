61651517b42e697f36506a27257e7a15f5f696a4
Delphine <delphine.vanrossum@student.uclouvain.be>
2026-01-09 18:35:55 +0100

    ex 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,10 +3,14 @@ import Person from "./components/Person";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", phone: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -16,21 +20,34 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
   const addPerson = (event) => {
     event.preventDefault();
     if (persons.find((person) => person.name === newName)) {
       alert(`${newName} is already added to phonebook`);
     } else {
-      const newPersonObj = { name: newName, phone: newNumber };
+      const newPersonObj = { name: newName, number: newNumber };
       setPersons(persons.concat(newPersonObj));
       setNewName("");
       setNewNumber("");
     }
   };
 
+  const personToShow = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input value={filter} onChange={handleFilterChange} />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -43,7 +60,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {personToShow.map((person) => (
         <Person key={person.name} person={person} />
       ))}
     </div>
