b99c10d5b7fa4c2c449335a68ca5bc47f8061997
Delphine <delphine.vanrossum@student.uclouvain.be>
2026-02-22 19:33:50 +0100

    ex3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,7 +3,13 @@ var morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :post"),
+);
+
+morgan.token("post", function (req, res) {
+  if (req.method === "POST") return JSON.stringify(req.body);
+});
 
 let persons = [
   {
